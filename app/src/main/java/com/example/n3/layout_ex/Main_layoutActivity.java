725c5a079f02d52ec2d1e34fb2362f0ad1868236
N3 <cabralycabral@hotmail.com>
2017-10-03 18:07:29 -0300

    Initial commit

---
+++ app/src/main/java/com/example/n3/layout_ex/Main_layoutActivity.java
@@ -0,0 +1,14 @@
+package com.example.n3.layout_ex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_layoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative_layout);
+
+    }
+}
